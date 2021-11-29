f77f1758721caa768c378ed796575a5e096bdf87
Flávio <flavio_r_o@yahoo.com.br>
2021-11-29 09:02:45 -0300

    Adiciona controller com ações para views

---
+++ sales-mvc/Controllers/SellersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sales_mvc.Services;
+
+namespace sales_mvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+        public IActionResult Index()
+        {
+            var list = _sellerService.FindAll();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
