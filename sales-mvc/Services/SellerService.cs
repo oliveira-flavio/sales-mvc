8ced08b4d0e669668917aa95ad58bc7bc802c752
Flávio <flavio_r_o@yahoo.com.br>
2021-11-30 16:42:20 -0300

    Adiciona funcão editar

--- sales-mvc/Services/SellerService.cs
+++ sales-mvc/Services/SellerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using sales_mvc.Data;
 using sales_mvc.Models;
+using sales_mvc.Services.Exceptions;
 
 namespace sales_mvc.Services
 {
@@ -26,13 +27,31 @@ namespace sales_mvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.Include(obj => obj.DepartmentId).FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
