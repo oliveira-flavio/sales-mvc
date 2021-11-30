a7dfa15355cd1defd9aef56439dd9a33dadd1126
Flávio <flavio_r_o@yahoo.com.br>
2021-11-30 16:42:10 -0300

    Adiciona exceções personalizadas

---
+++ sales-mvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace sales_mvc.Services.Exceptions
+{
+    public class NotFoundException : ApplicationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
